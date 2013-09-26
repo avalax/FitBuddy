37228156a5b0c764a6300c7d47b7a9c8934ba78b
Benjamin <mette@avalax.de>
2013-09-26 21:19:53 +0200

    Bootstrap

---
+++ src/main/java/de/avalax/trainee/HelloAndroidActivity.java
@@ -0,0 +1,18 @@
+package de.avalax.trainee;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+
+@ContentView(R.layout.activity_main)
+public class HelloAndroidActivity extends RoboActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
+
