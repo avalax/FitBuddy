23a8f7952bd206b77b0057ce513352b46d10d72a
Benjamin <mette@avalax.de>
2013-09-29 14:58:04 +0200

    removed setMaxRepetitions

--- src/main/java/de/avalax/fitbuddy/workout/Set.java
+++ src/main/java/de/avalax/fitbuddy/workout/Set.java
@@ -2,10 +2,7 @@ package de.avalax.fitbuddy.workout;
 
 public interface Set {
     double getWeight();
-    void setRepetitions(int repetitions);
     int getRepetitions();
-
-		void setMaxRepetitions(int maxRepetitions);
-
-		int getMaxRepetition();
+    void setRepetitions(int repetitions);
+    int getMaxRepetition();
 }
