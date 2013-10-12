1032d1b4702857ba24569acebd7b243e85e3472f
Benjamin <github@avalax.de>
2013-10-12 22:23:56 +0200

    added some variation for DummyDataLayer

--- src/main/java/de/avalax/fitbuddy/workout/persistence/DummyDataLayer.java
+++ src/main/java/de/avalax/fitbuddy/workout/persistence/DummyDataLayer.java
@@ -13,25 +13,25 @@ import java.util.List;
 public class DummyDataLayer implements DataLayer {
     @Override
     public Workout load() {
-        List<Exercise> exercises = new ArrayList<Exercise>();
-        exercises.add(new BasicExercise("Bankdrücken", createSetWithThreeSets(70), 5));
-        exercises.add(new BasicExercise("Schrägbankdrücken", createSetWithThreeSets(40), 5));
-        exercises.add(new BasicExercise("Butterfly", createSetWithThreeSets(60), 5));
-        exercises.add(new BasicExercise("Liegestüze", createSetWithThreeSets(0), 12.5));
-        exercises.add(new BasicExercise("Lat-Ziehen hinter den Nacken", createSetWithThreeSets(50), 5));
-        exercises.add(new BasicExercise("Hochziehen der Langhantel mit enger Handstellung", createSetWithThreeSets(10), 2.5));
-        exercises.add(new BasicExercise("Klimmzüge", createSetWithThreeSets(60), 5));
-        exercises.add(new BasicExercise("Hammercurls", createSetWithThreeSets(30), 2.5));
-        exercises.add(new BasicExercise("Larry Scoot Armbeugen", createSetWithThreeSets(30), 2.5));
-        exercises.add(new BasicExercise("Situps", createSetWithThreeSets(0), 2.5));
+        List<Exercise> exercises = new ArrayList<>();
+        exercises.add(new BasicExercise("Bankdrücken", createSetWithThreeSets(70,12), 5));
+        exercises.add(new BasicExercise("Schrägbankdrücken", createSetWithThreeSets(40,10), 5));
+        exercises.add(new BasicExercise("Butterfly", createSetWithThreeSets(60,15), 5));
+        exercises.add(new BasicExercise("Liegestüze", createSetWithThreeSets(0,30), 12.5));
+        exercises.add(new BasicExercise("Lat-Ziehen hinter den Nacken", createSetWithThreeSets(50,12), 5));
+        exercises.add(new BasicExercise("Hochziehen der Langhantel mit enger Handstellung", createSetWithThreeSets(10,12), 2.5));
+        exercises.add(new BasicExercise("Klimmzüge", createSetWithThreeSets(60,8), 5));
+        exercises.add(new BasicExercise("Hammercurls", createSetWithThreeSets(30,20), 2.5));
+        exercises.add(new BasicExercise("Larry Scoot Armbeugen", createSetWithThreeSets(30,12), 2.5));
+        exercises.add(new BasicExercise("Situps", createSetWithThreeSets(0,30), 2.5));
         return new BasicWorkout(exercises);
     }
 
-    private List<Set> createSetWithThreeSets(double weight) {
+    private List<Set> createSetWithThreeSets(double weight,int reps) {
         List<Set> sets = new ArrayList<>();
-        sets.add(new BasicSet(weight, 15));
-        sets.add(new BasicSet(weight, 15));
-        sets.add(new BasicSet(weight, 15));
+        sets.add(new BasicSet(weight, reps));
+        sets.add(new BasicSet(weight, reps));
+        sets.add(new BasicSet(weight, reps));
         return sets;
     }
 }
