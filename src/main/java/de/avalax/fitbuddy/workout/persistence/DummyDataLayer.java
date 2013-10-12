c616ea76085979abd32115837ae3d7b427d766c0
Benjamin <github@avalax.de>
2013-10-12 16:38:04 +0200

    added some more exercises to the dummy datalayer

--- src/main/java/de/avalax/fitbuddy/workout/persistence/DummyDataLayer.java
+++ src/main/java/de/avalax/fitbuddy/workout/persistence/DummyDataLayer.java
@@ -15,12 +15,20 @@ public class DummyDataLayer implements DataLayer {
     public Workout load() {
         List<Exercise> exercises = new ArrayList<Exercise>();
         exercises.add(new BasicExercise("Bankdrücken", createSetWithThreeSets(70), 5));
-        exercises.add(new BasicExercise("Situps", createSetWithThreeSets(1.5), 2.5));
+        exercises.add(new BasicExercise("Schrägbankdrücken", createSetWithThreeSets(40), 5));
+        exercises.add(new BasicExercise("Butterfly", createSetWithThreeSets(60), 5));
+        exercises.add(new BasicExercise("Liegestüze", createSetWithThreeSets(0), 12.5));
+        exercises.add(new BasicExercise("Lat-Ziehen hinter den Nacken", createSetWithThreeSets(50), 5));
+        exercises.add(new BasicExercise("Hochziehen der Langhantel mit enger Handstellung", createSetWithThreeSets(10), 2.5));
+        exercises.add(new BasicExercise("Klimmzüge", createSetWithThreeSets(60), 5));
+        exercises.add(new BasicExercise("Hammercurls", createSetWithThreeSets(30), 2.5));
+        exercises.add(new BasicExercise("Larry Scoot Armbeugen", createSetWithThreeSets(30), 2.5));
+        exercises.add(new BasicExercise("Situps", createSetWithThreeSets(0), 2.5));
         return new BasicWorkout(exercises);
     }
 
     private List<Set> createSetWithThreeSets(double weight) {
-        List<Set> sets = new ArrayList<Set>();
+        List<Set> sets = new ArrayList<>();
         sets.add(new BasicSet(weight, 15));
         sets.add(new BasicSet(weight, 15));
         sets.add(new BasicSet(weight, 15));
