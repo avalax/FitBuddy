390bb566478d1c8d46ae7d8498c6d5617ac9d387
Benjamin <github@avalax.de>
2013-10-19 17:48:27 +0200

    refacotring + autoformat

--- src/main/java/de/avalax/fitbuddy/progressBar/HorizontalProgressBar.java
+++ src/main/java/de/avalax/fitbuddy/progressBar/HorizontalProgressBar.java
@@ -10,95 +10,100 @@ import de.avalax.fitbuddy.workout.Exercise;
 import de.avalax.fitbuddy.workout.Set;
 
 public class HorizontalProgressBar extends View {
-	private Typeface mTypeface  = Typeface.create("sans", Typeface.BOLD);
-	private int textColor;
-	private int barColor;
-	private float textSize;
-	private int currentValue;
-	private int maxValue;
+    private Typeface mTypeface = Typeface.create("sans", Typeface.BOLD);
+    private int textColor;
+    private int barColor;
+    private float textSize;
+    private int currentValue;
+    private int maxValue;
 
-	public HorizontalProgressBar(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		setAttrs(attrs);
-	}
+    public HorizontalProgressBar(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        setAttributeSet(attributeSet);
+    }
 
-	private void setAttrs(AttributeSet attrs) {
-		if (attrs != null) {
-			TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.HorizontalProgressBar);
-			setTextColor(a.getColor(R.styleable.HorizontalProgressBar_textColor, Color.parseColor("#ffffff")));
-			setTextSize(a.getDimension(R.styleable.HorizontalProgressBar_textSize, 50));
-			setBarColor(a.getColor(R.styleable.HorizontalProgressBar_barColor, Color.parseColor("#CC0000")));
-			setCurrentValue(a.getInt(R.styleable.HorizontalProgressBar_current_value, 0));
-			setMaxValue(a.getInt(R.styleable.HorizontalProgressBar_max_value,0));
-			a.recycle();
-		}
-	}
+    private void setAttributeSet(AttributeSet attributeSet) {
+        if (attributeSet == null) {
+            return;
+        }
+        TypedArray a = getContext().obtainStyledAttributes(attributeSet, R.styleable.HorizontalProgressBar);
+        setTextColor(a.getColor(R.styleable.HorizontalProgressBar_textColor, Color.parseColor("#ffffff")));
+        setTextSize(a.getDimension(R.styleable.HorizontalProgressBar_textSize, 50));
+        setBarColor(a.getColor(R.styleable.HorizontalProgressBar_barColor, Color.parseColor("#CC0000")));
+        setCurrentValue(a.getInt(R.styleable.HorizontalProgressBar_current_value, 0));
+        setMaxValue(a.getInt(R.styleable.HorizontalProgressBar_max_value, 0));
+        a.recycle();
+    }
 
-	@Override
-	protected synchronized void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-		DrawProgressBar(canvas);
-		DrawTextInProgressBar(canvas);
-	}
+    @Override
+    protected synchronized void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        DrawProgressBar(canvas);
+        DrawTextInProgressBar(canvas);
+    }
 
-	private void DrawProgressBar(Canvas canvas){
-		Paint contentPaint = new Paint();
-		contentPaint.setAntiAlias(true);
-		int height = CalculateProgressBarHeight();
-		Rect progressBody = new Rect(0, getHeight() - height, this.getWidth(), getHeight());
-		contentPaint.setStyle(Paint.Style.FILL);
-		contentPaint.setColor(barColor);
-		canvas.drawRect(progressBody, contentPaint);
-	}
+    private void DrawProgressBar(Canvas canvas) {
+        Paint contentPaint = new Paint();
+        contentPaint.setAntiAlias(true);
+        int height = CalculateProgressBarHeight();
+        Rect progressBody = new Rect(0, getHeight() - height, this.getWidth(), getHeight());
+        contentPaint.setStyle(Paint.Style.FILL);
+        contentPaint.setColor(barColor);
+        canvas.drawRect(progressBody, contentPaint);
+    }
 
-	private int CalculateProgressBarHeight(){
-		float scale = (float)currentValue / (float)maxValue;
-		return (int)(scale *  this.getHeight());
-	}
+    private int CalculateProgressBarHeight() {
+        float scale = (float) currentValue / (float) maxValue;
+        return (int) (scale * this.getHeight());
+    }
 
-	private void DrawTextInProgressBar(Canvas canvas){
-		Paint textPaint = new Paint();
-		textPaint.setAntiAlias(true);
-		textPaint.setColor(textColor);
-		textPaint.setTextSize(textSize);
-		textPaint.setTypeface(mTypeface);
-		String textCurrentValue = currentValue+"";
-		String textMaxValue = maxValue+"";
-		DrawTextDimensionValues drawTextDimensionValuesCurrentValue = new DrawTextDimensionValues(textPaint, textCurrentValue, getWidth(), getHeight()).invoke();
-		DrawTextDimensionValues drawTextDimensionValuesMaxValue = new DrawTextDimensionValues(textPaint,  textMaxValue, getWidth(), getHeight()).invoke();
-		canvas.drawText(textCurrentValue, drawTextDimensionValuesCurrentValue.getX(), drawTextDimensionValuesCurrentValue.getY(), textPaint);
-		canvas.drawText(textMaxValue, drawTextDimensionValuesMaxValue.getX(), drawTextDimensionValuesMaxValue.getY()+textSize, textPaint);
+    private void DrawTextInProgressBar(Canvas canvas) {
+        Paint textPaint = new Paint();
+        textPaint.setAntiAlias(true);
+        textPaint.setColor(textColor);
+        textPaint.setTextSize(textSize);
+        textPaint.setTypeface(mTypeface);
+        String textCurrentValue = currentValue + "";
+        String textMaxValue = maxValue + "";
+        DrawTextDimensionValues drawTextDimensionValuesCurrentValue = new DrawTextDimensionValues(textPaint, textCurrentValue, getWidth(), getHeight()).invoke();
+        DrawTextDimensionValues drawTextDimensionValuesMaxValue = new DrawTextDimensionValues(textPaint, textMaxValue, getWidth(), getHeight()).invoke();
+        canvas.drawText(textCurrentValue, drawTextDimensionValuesCurrentValue.getX(), drawTextDimensionValuesCurrentValue.getY(), textPaint);
+        canvas.drawText(textMaxValue, drawTextDimensionValuesMaxValue.getX(), drawTextDimensionValuesMaxValue.getY() + textSize, textPaint);
 
-	}
+    }
 
-	public synchronized void setTextColor(int textColor) {
-		this.textColor = textColor;
-		postInvalidate();
-	}
-	public void setProgressBar(Exercise exercise){
-		setCurrentValue(exercise.getSetNumber());
-		setMaxValue(exercise.getSetSize());
-	}
-	public void setProgressBar(Set set){
-		setCurrentValue(set.getReps());
-		setMaxValue(set.getRepsSize());
-	}
-	public synchronized void setBarColor(int barColor) {
-		this.barColor = barColor;
-		postInvalidate();
-	}
-	public synchronized void setMaxValue(int reps) {
-		this.maxValue = reps;
-		postInvalidate();
-	}
+    public synchronized void setTextColor(int textColor) {
+        this.textColor = textColor;
+        postInvalidate();
+    }
 
-	public synchronized void setCurrentValue(int reps) {
-		this.currentValue = reps;
-		postInvalidate();
-	}
+    public void setProgressBar(Exercise exercise) {
+        setCurrentValue(exercise.getSetNumber());
+        setMaxValue(exercise.getSetSize());
+    }
 
-	public synchronized void setTextSize(float textSize) {
-		this.textSize = textSize;
-		postInvalidate();
-	}
+    public void setProgressBar(Set set) {
+        setCurrentValue(set.getReps());
+        setMaxValue(set.getRepsSize());
+    }
+
+    public synchronized void setBarColor(int barColor) {
+        this.barColor = barColor;
+        postInvalidate();
+    }
+
+    public synchronized void setMaxValue(int reps) {
+        this.maxValue = reps;
+        postInvalidate();
+    }
+
+    public synchronized void setCurrentValue(int reps) {
+        this.currentValue = reps;
+        postInvalidate();
+    }
+
+    public synchronized void setTextSize(float textSize) {
+        this.textSize = textSize;
+        postInvalidate();
+    }
 }
