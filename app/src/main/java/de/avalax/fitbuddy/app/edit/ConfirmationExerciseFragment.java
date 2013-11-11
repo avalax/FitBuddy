e6ca7c9cd76d5d7e9dc095dd612d415afa79e740
Benjamin <github@avalax.de>
2013-11-11 23:03:10 +0100

    fixed missing layout for confirmation exercise

--- app/src/main/java/de/avalax/fitbuddy/app/edit/ConfirmationExerciseFragment.java
+++ app/src/main/java/de/avalax/fitbuddy/app/edit/ConfirmationExerciseFragment.java
@@ -3,16 +3,16 @@ package de.avalax.fitbuddy.app.edit;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import de.avalax.fitbuddy.app.R;
 import de.avalax.fitbuddy.app.UpdateableActivity;
 import roboguice.fragment.RoboFragment;
-import roboguice.inject.ContentView;
 import roboguice.inject.InjectView;
 
-@ContentView(R.layout.edit_confirmation)
 public class ConfirmationExerciseFragment extends RoboFragment {
     private EditableExercise editableExercise;
 
@@ -22,6 +22,13 @@ public class ConfirmationExerciseFragment extends RoboFragment {
     @InjectView(R.id.buttonCancel)
     Button buttonCancel;
     private UpdateableActivity updateableActivity;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.edit_confirmation, container, false);
+    }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
