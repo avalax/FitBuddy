ff5f5a702597234197ecae5bc7b7f5264ace0db3
Benjamin <github@avalax.de>
2013-11-13 23:40:04 +0100

    #6 fragment_result on result screen

--- app/src/main/java/de/avalax/fitbuddy/app/WorkoutResultFragment.java
+++ app/src/main/java/de/avalax/fitbuddy/app/WorkoutResultFragment.java
@@ -12,6 +12,6 @@ public class WorkoutResultFragment extends RoboFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.workout_result, container, false);
+        return inflater.inflate(R.layout.fragment_result, container, false);
     }
 }
