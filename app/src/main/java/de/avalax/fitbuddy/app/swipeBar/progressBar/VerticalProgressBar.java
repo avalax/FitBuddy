37ed592c5cc2ebc2b01a0789977584dc13e2ccd3
Benjamin <github@avalax.de>
2013-11-26 00:09:20 +0100

    #21 verticalprogressbar drawn via xml

--- app/src/main/java/de/avalax/fitbuddy/app/swipeBar/progressBar/VerticalProgressBar.java
+++ app/src/main/java/de/avalax/fitbuddy/app/swipeBar/progressBar/VerticalProgressBar.java
@@ -1,114 +1,73 @@
 package de.avalax.fitbuddy.app.swipeBar.progressBar;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.*;
 import android.util.AttributeSet;
-import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 import de.avalax.fitbuddy.app.R;
 import de.avalax.fitbuddy.app.swipeBar.SwipeableBar;
 import de.avalax.fitbuddy.core.workout.Exercise;
 import de.avalax.fitbuddy.core.workout.Set;
 import de.avalax.fitbuddy.core.workout.Workout;
 
-public class VerticalProgressBar extends View implements SwipeableBar {
-    private Typeface typeface = Typeface.create("sans", Typeface.BOLD);
-    private int textColor;
+public class VerticalProgressBar extends FrameLayout implements SwipeableBar {
     private int barColor;
-    private float textSize;
     private float currentValue;
     private float maxValue;
-    private String currentTextValue;
-    private String maxTextValue;
+    private TextView maxValueTextView;
+    private TextView valueTextView;
+    private XmlVerticalProgressBar xmlVerticalProgressBar;
 
-    public VerticalProgressBar(Context context, AttributeSet attributeSet) {
-        super(context, attributeSet);
-        setAttributeSet(attributeSet);
+    public VerticalProgressBar(Context context) {
+        super(context);
+        init(context);
     }
 
-    private void setAttributeSet(AttributeSet attributeSet) {
-        if (attributeSet == null) {
-            return;
-        }
-        TypedArray a = getContext().obtainStyledAttributes(attributeSet, R.styleable.ProgressBar);
-        setTextColor(a.getColor(R.styleable.ProgressBar_textColor, Color.parseColor("#ffffff")));
-        setTextSize(a.getDimension(R.styleable.ProgressBar_textSize, 50));
-        setBarColor(a.getColor(R.styleable.ProgressBar_barColor, Color.parseColor("#CC0000")));
-        setCurrentValue(a.getInt(R.styleable.ProgressBar_currentValue, 0));
-        setMaxValue(a.getInt(R.styleable.ProgressBar_maxValue, 0));
-        a.recycle();
+    public VerticalProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public VerticalProgressBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
+
+    private void init(Context context) {
+        inflate(context, R.layout.vertical_bar, this);
+        maxValueTextView = (TextView) findViewById(R.id.maxValueTextView);
+        valueTextView = (TextView) findViewById(R.id.valueTextView);
+        xmlVerticalProgressBar = (XmlVerticalProgressBar) findViewById(R.id.verticalProgressBar);
+        xmlVerticalProgressBar.setBackgroundColor(R.styleable.ProgressBar_barColor);
+        //TODO: progressbar color
+
+        this.currentValue = 0;
+        this.maxValue = 0;
+        valueTextView.setText(String.valueOf(currentValue));
+        maxValueTextView.setText(String.valueOf(maxValue));
     }
 
     public void setProgressBar(Workout workout, int exercisePosition) {
         Exercise exercise = workout.getExercise(exercisePosition);
         this.currentValue = workout.getProgress(exercisePosition);
-        this.currentTextValue = String.valueOf(exercise.getSetNumber());
         this.maxValue = workout.getExerciseCount();
-        this.maxTextValue = String.valueOf(exercise.getMaxSets());
+        valueTextView.setText(String.valueOf(exercise.getSetNumber()));
+        maxValueTextView.setText(String.valueOf(exercise.getMaxSets()));
+        xmlVerticalProgressBar.setImageLevel(calculateProgressBarHeight());
         postInvalidate();
     }
 
     public void setProgressBar(Set set) {
-        setCurrentValue(set.getReps());
-        setMaxValue(set.getMaxReps());
+        this.currentValue = set.getReps();
+        this.maxValue = set.getMaxReps();
+        valueTextView.setText(String.valueOf(set.getReps()));
+        maxValueTextView.setText(String.valueOf(set.getMaxReps()));
+        xmlVerticalProgressBar.setImageLevel(calculateProgressBarHeight());
         postInvalidate();
-    }
-
-    @Override
-    protected synchronized void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        drawProgressBar(canvas);
-        drawTextInProgressBar(canvas);
-    }
-
-    private void drawProgressBar(Canvas canvas) {
-        Paint contentPaint = new Paint();
-        contentPaint.setAntiAlias(true);
-        contentPaint.setStyle(Paint.Style.FILL);
-        contentPaint.setColor(barColor);
-        int height = calculateProgressBarHeight();
-        Rect progressBody = new Rect(0, getHeight() - height, this.getWidth(), getHeight());
-        canvas.drawRect(progressBody, contentPaint);
     }
 
     private int calculateProgressBarHeight() {
         float scale = currentValue / maxValue;
-        return Math.round(scale * this.getHeight());
-    }
-
-    private void drawTextInProgressBar(Canvas canvas) {
-        Paint textPaint = new Paint();
-        textPaint.setAntiAlias(true);
-        textPaint.setColor(textColor);
-        textPaint.setTextSize(textSize);
-        textPaint.setTypeface(typeface);
-        String textCurrentValue = currentTextValue;
-        String textMaxValue = maxTextValue;
-        DrawTextDimensionValues drawTextDimensionValuesCurrentValue = new DrawTextDimensionValues(textPaint, textCurrentValue, getWidth(), getHeight()).invoke();
-        DrawTextDimensionValues drawTextDimensionValuesMaxValue = new DrawTextDimensionValues(textPaint, textMaxValue, getWidth(), getHeight()).invoke();
-        canvas.drawText(textCurrentValue, drawTextDimensionValuesCurrentValue.getX(), drawTextDimensionValuesCurrentValue.getY(), textPaint);
-        canvas.drawText(textMaxValue, drawTextDimensionValuesMaxValue.getX(), drawTextDimensionValuesMaxValue.getY() + textSize, textPaint);
-    }
-
-    private synchronized void setTextColor(int textColor) {
-        this.textColor = textColor;
-    }
-
-    private synchronized void setBarColor(int barColor) {
-        this.barColor = barColor;
-    }
-
-    private synchronized void setMaxValue(int maxValue) {
-        this.maxValue = maxValue;
-        this.maxTextValue = String.valueOf(maxValue);
-    }
-
-    private synchronized void setCurrentValue(int currentValue) {
-        this.currentValue = currentValue;
-        this.currentTextValue = String.valueOf(currentValue);
-    }
-
-    private synchronized void setTextSize(float textSize) {
-        this.textSize = textSize;
+        return Math.round(scale * 10000);
     }
 }
