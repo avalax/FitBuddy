efe2fefab849ab7e4c8ee1d7235623a8c3c188b5
Benjamin <github@avalax.de>
2013-11-26 23:05:12 +0100

    #21 finished VerticalProgressBar

--- app/src/main/java/de/avalax/fitbuddy/app/swipeBar/progressBar/VerticalProgressBar.java
+++ app/src/main/java/de/avalax/fitbuddy/app/swipeBar/progressBar/VerticalProgressBar.java
@@ -1,8 +1,10 @@
 package de.avalax.fitbuddy.app.swipeBar.progressBar;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 import de.avalax.fitbuddy.app.R;
 import de.avalax.fitbuddy.app.swipeBar.SwipeableBar;
@@ -11,12 +13,9 @@ import de.avalax.fitbuddy.core.workout.Set;
 import de.avalax.fitbuddy.core.workout.Workout;
 
 public class VerticalProgressBar extends FrameLayout implements SwipeableBar {
-    private int barColor;
-    private float currentValue;
-    private float maxValue;
     private TextView maxValueTextView;
     private TextView valueTextView;
-    private XmlVerticalProgressBar xmlVerticalProgressBar;
+    private ImageView imageView;
 
     public VerticalProgressBar(Context context) {
         super(context);
@@ -26,47 +25,52 @@ public class VerticalProgressBar extends FrameLayout implements SwipeableBar {
     public VerticalProgressBar(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(context);
+        attributs(attrs);
     }
 
     public VerticalProgressBar(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         init(context);
+        attributs(attrs);
+    }
+
+    private void attributs(AttributeSet attrs) {
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.ProgressBar);
+        imageView.setImageDrawable(a.getDrawable(R.styleable.ProgressBar_progressbar));
+        imageView.setBackgroundColor(a.getColor(R.styleable.ProgressBar_background, R.color.primary_background));
+        a.recycle();
     }
 
     private void init(Context context) {
         inflate(context, R.layout.vertical_bar, this);
         maxValueTextView = (TextView) findViewById(R.id.maxValueTextView);
         valueTextView = (TextView) findViewById(R.id.valueTextView);
-        xmlVerticalProgressBar = (XmlVerticalProgressBar) findViewById(R.id.verticalProgressBar);
-        xmlVerticalProgressBar.setBackgroundColor(R.styleable.ProgressBar_barColor);
-        //TODO: progressbar color
-
-        this.currentValue = 0;
-        this.maxValue = 0;
-        valueTextView.setText(String.valueOf(currentValue));
-        maxValueTextView.setText(String.valueOf(maxValue));
+        imageView = (ImageView) findViewById(R.id.verticalProgressBar);
     }
 
     public void setProgressBar(Workout workout, int exercisePosition) {
         Exercise exercise = workout.getExercise(exercisePosition);
-        this.currentValue = workout.getProgress(exercisePosition);
-        this.maxValue = workout.getExerciseCount();
-        valueTextView.setText(String.valueOf(exercise.getSetNumber()));
-        maxValueTextView.setText(String.valueOf(exercise.getMaxSets()));
-        xmlVerticalProgressBar.setImageLevel(calculateProgressBarHeight());
-        postInvalidate();
+        float currentValue = workout.getProgress(exercisePosition);
+        int maxValue = workout.getExerciseCount();
+        String currentValueText = String.valueOf(exercise.getSetNumber());
+        String maxValueText = String.valueOf(exercise.getMaxSets());
+        updateProgressBar(currentValue, maxValue, currentValueText, maxValueText);
     }
 
     public void setProgressBar(Set set) {
-        this.currentValue = set.getReps();
-        this.maxValue = set.getMaxReps();
-        valueTextView.setText(String.valueOf(set.getReps()));
-        maxValueTextView.setText(String.valueOf(set.getMaxReps()));
-        xmlVerticalProgressBar.setImageLevel(calculateProgressBarHeight());
+        int currentValue = set.getReps();
+        int maxValue = set.getMaxReps();
+        updateProgressBar(currentValue, maxValue, String.valueOf(currentValue), String.valueOf(maxValue));
+    }
+
+    private void updateProgressBar(float currentValue, int maxValue, String currentValueText, String maxValueText) {
+        valueTextView.setText(currentValueText);
+        maxValueTextView.setText(maxValueText);
+        imageView.setImageLevel(calculateProgressBarHeight(currentValue, maxValue));
         postInvalidate();
     }
 
-    private int calculateProgressBarHeight() {
+    private int calculateProgressBarHeight(float currentValue, int maxValue) {
         float scale = currentValue / maxValue;
         return Math.round(scale * 10000);
     }
