ddf9f527a84aba1384bea99fb5d311447da88173
Benjamin <github@avalax.de>
2013-12-13 20:08:30 +0100

    #6 added viewflipper for workout result

--- app/src/main/java/de/avalax/fitbuddy/app/WorkoutResultFragment.java
+++ app/src/main/java/de/avalax/fitbuddy/app/WorkoutResultFragment.java
@@ -2,16 +2,53 @@ package de.avalax.fitbuddy.app;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ViewFlipper;
+import com.google.inject.Inject;
+import de.avalax.fitbuddy.core.workout.Workout;
 import roboguice.fragment.RoboFragment;
+import roboguice.inject.InjectView;
 
 public class WorkoutResultFragment extends RoboFragment {
-
+    @InjectView(R.id.resultChartViewFlipper)
+    private ViewFlipper resultChartViewFlipper;
+    @Inject
+    private Workout workout;
+    @Inject
+    private LayoutInflater layoutInflater;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_result, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViewFlipper();
+        fillListView();
+    }
+
+    private void initViewFlipper() {
+        resultChartViewFlipper.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                resultChartViewFlipper.setInAnimation(getActivity(), R.anim.out_to_top);
+                resultChartViewFlipper.setOutAnimation(getActivity(), R.anim.in_from_bottom);
+                resultChartViewFlipper.showNext();
+                return true;
+            }
+        });
+    }
+
+    private void fillListView() {
+        for (int i = 0; i < workout.getExerciseCount(); i++) {
+            View inflate = layoutInflater.inflate(R.layout.view_exercise_result, null);
+            resultChartViewFlipper.addView(inflate);
+            //exercices.add(workout.getExercise(i));
+        }
     }
 }
--- core/src/main/java/de/avalax/fitbuddy/core/workout/persistence/DummyDataLayer.java
+++ core/src/main/java/de/avalax/fitbuddy/core/workout/persistence/DummyDataLayer.java
@@ -17,14 +17,14 @@ public class DummyDataLayer implements DataLayer {
         LinkedList<Exercise> exercises = new LinkedList<Exercise>();
         exercises.add(new BasicExercise("Bankdrücken", createExerciseWithThreeSets(70, 12), 5));
         exercises.add(new BasicExercise("Schrägbankdrücken", createExerciseWithThreeSets(40, 10), 5));
-        exercises.add(new BasicExercise("Butterfly", createExerciseWithThreeSets(60, 15), 5));
+        /**exercises.add(new BasicExercise("Butterfly", createExerciseWithThreeSets(60, 15), 5));
         exercises.add(new BasicExercise("Liegestüze", createExerciseWithThreeSets(0, 30), 12.5));
         exercises.add(new BasicExercise("Lat-Ziehen hinter den Nacken", createExerciseWithThreeSets(50, 12), 5));
         exercises.add(new BasicExercise("Hochziehen der Langhantel mit enger Handstellung", createExerciseWithThreeSets(10, 12), 2.5));
         exercises.add(new BasicExercise("Klimmzüge", createExerciseWithThreeSets(60, 8), 5));
         exercises.add(new BasicExercise("Hammercurls", createExerciseWithThreeSets(30, 20), 2.5));
         exercises.add(new BasicExercise("Larry Scoot Armbeugen", createExerciseWithThreeSets(30, 12), 2.5));
-        exercises.add(new BasicExercise("Situps", createExerciseWithThreeSets(0, 30), 2.5));
+        exercises.add(new BasicExercise("Situps", createExerciseWithThreeSets(0, 30), 2.5));  */
         return new BasicWorkout(exercises);
     }
 
