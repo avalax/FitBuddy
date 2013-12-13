c2b086850b0046d2bdb595eda57966d3eecad05b
Benjamin <github@avalax.de>
2013-12-13 23:19:57 +0100

    refactoring

--- app/src/main/java/de/avalax/fitbuddy/app/swipeBar/SwipeBarOnTouchListener.java
+++ app/src/main/java/de/avalax/fitbuddy/app/swipeBar/SwipeBarOnTouchListener.java
@@ -1,28 +1,21 @@
 package de.avalax.fitbuddy.app.swipeBar;
 
 import android.content.Context;
-import android.support.v4.view.VelocityTrackerCompat;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
-import android.view.VelocityTracker;
 import android.view.View;
 import android.view.ViewConfiguration;
 
 public abstract class SwipeBarOnTouchListener implements View.OnTouchListener {
-    private int minSwipeDistance;
-    private int thresholdSwipeVelocity;
-		private ViewConfiguration viewConfiguration;
-
     private final GestureDetector gdt;
 
     public SwipeBarOnTouchListener(Context context, SwipeableBar swipeableBar, int swipeMoveMax) {
         final SwipeBarOnTouchListener touchListener = this;
-	      viewConfiguration = ViewConfiguration.get(context);
-	      minSwipeDistance = viewConfiguration.getScaledPagingTouchSlop();
-	      thresholdSwipeVelocity = viewConfiguration.getScaledMinimumFlingVelocity();
+        ViewConfiguration viewConfiguration = ViewConfiguration.get(context);
+        int minSwipeDistance = viewConfiguration.getScaledPagingTouchSlop();
+        int thresholdSwipeVelocity = viewConfiguration.getScaledMinimumFlingVelocity();
 
-        gdt = new GestureDetector(context,new SwipeBarOnGestureListener(swipeMoveMax, swipeableBar, minSwipeDistance, thresholdSwipeVelocity) {
+        gdt = new GestureDetector(context, new SwipeBarOnGestureListener(swipeMoveMax, swipeableBar, minSwipeDistance, thresholdSwipeVelocity) {
             @Override
             public void onFlingEvent(int moved) {
                 touchListener.onFlingEvent(moved);
