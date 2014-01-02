6a890cec41100c234c1a72d43e9e7f060fce93ad
Benjamin <github@avalax.de>
2014-01-03 00:02:57 +0100

    #22 extract texts to strings.xml

--- app/src/main/java/de/avalax/fitbuddy/app/edit/ConfirmationExerciseFragment.java
+++ app/src/main/java/de/avalax/fitbuddy/app/edit/ConfirmationExerciseFragment.java
@@ -22,7 +22,7 @@ public class ConfirmationExerciseFragment extends RoboFragment {
     private Button buttonCancel;
     @InjectResource(R.string.new_exercise_name)
     private String newExerciseName;
-    @InjectResource(R.string.edit_button_delete_exercise_text)
+    @InjectResource(R.string.action_delete)
     private String buttonDeleteExerciseText;
     private EditableExercise editableExercise;
     private UpdateableActivity updateableActivity;
