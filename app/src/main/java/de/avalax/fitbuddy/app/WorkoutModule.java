2882b4d39ec66f65085ea803eb366ccfc9830aeb
Benjamin <github@avalax.de>
2014-01-12 21:45:02 +0100

    readded singleton for workout and datalayer

--- app/src/main/java/de/avalax/fitbuddy/app/WorkoutModule.java
+++ app/src/main/java/de/avalax/fitbuddy/app/WorkoutModule.java
@@ -1,6 +1,7 @@
 package de.avalax.fitbuddy.app;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
 import de.avalax.fitbuddy.core.workout.Workout;
 import de.avalax.fitbuddy.datalayer.DataLayer;
 import de.avalax.fitbuddy.datalayer.DummyDataLayer;
@@ -9,9 +10,9 @@ import de.avalax.fitbuddy.datalayer.PersistenceWorkout;
 public class WorkoutModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(DataLayer.class).to(DummyDataLayer.class);
+        bind(DataLayer.class).to(DummyDataLayer.class).in(Singleton.class);
         try {
-            bind(Workout.class).toConstructor(PersistenceWorkout.class.getConstructor(DataLayer.class));
+            bind(Workout.class).toConstructor(PersistenceWorkout.class.getConstructor(DataLayer.class)).in(Singleton.class);
         } catch (NoSuchMethodException e) {
             addError(e);
         }
