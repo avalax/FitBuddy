917b7934973c5916c89c931dfc99da3fdf34cee7
Benjamin <github@avalax.de>
2014-03-08 09:06:38 +0100

    FakeWorkoutDAO more dynamic

--- datalayer/src/main/java/de/avalax/fitbuddy/datalayer/FakeWorkoutDAO.java
+++ datalayer/src/main/java/de/avalax/fitbuddy/datalayer/FakeWorkoutDAO.java
@@ -13,49 +13,65 @@ import java.util.List;
 
 public class FakeWorkoutDAO implements WorkoutDAO {
 
+    private ArrayList<Workout> workouts;
+
+    public FakeWorkoutDAO() {
+        workouts = new ArrayList<>();
+        workouts.add(fakeWorkoutOne());
+        workouts.add(fakeWorkoutTwo());
+    }
+
     @Override
     public void save(Workout workout) {
-        //Nothing to do here, it's just a static workout ;-)
+        if (!workouts.contains(workout)) {
+            workouts.add(workout);
+        }
     }
 
     @Override
     public Workout load(int position) {
-        if (position%2 == 0) {
-            LinkedList<Exercise> exercises = new LinkedList<>();
-            exercises.add(new BasicExercise("Bankdrücken", createExerciseWithThreeSets(70, 12), 5));
-            exercises.add(new BasicExercise("Schrägbankdrücken", createExerciseWithThreeSets(40, 10), 5));
-            exercises.add(new BasicExercise("Butterfly", createExerciseWithThreeSets(60, 15), 5));
-            exercises.add(new BasicExercise("Liegestütze", createExerciseWithThreeSets(0, 30), 12.5));
-            exercises.add(new BasicExercise("Lat-Ziehen hinter den Nacken", createExerciseWithThreeSets(50, 12), 5));
-            exercises.add(new BasicExercise("Hochziehen der Langhantel mit enger Handstellung", createExerciseWithThreeSets(10, 12), 2.5));
-            exercises.add(new BasicExercise("Klimmzüge", createExerciseWithThreeSets(60, 8), 5));
-            exercises.add(new BasicExercise("Hammercurls", createExerciseWithThreeSets(30, 20), 2.5));
-            exercises.add(new BasicExercise("Larry Scoot Armbeugen", createExerciseWithThreeSets(30, 12), 2.5));
-            exercises.add(new BasicExercise("Situps", createExerciseWithThreeSets(0, 30), 2.5));
-            return new BasicWorkout(exercises);
-        } else {
-            LinkedList<Exercise> exercises = new LinkedList<>();
-            exercises.add(new BasicExercise("Bankdrücken", createExerciseWithThreeSets(50, 12), 5));
-            exercises.add(new BasicExercise("KH-Rudern", createExerciseWithThreeSets(10, 12), 2.5));
-            exercises.add(new BasicExercise("KH-Latissimus", createExerciseWithThreeSets(10, 12), 2.5));
-            exercises.add(new BasicExercise("Triceps am hohen Block", createExerciseWithThreeSets(25, 12), 5));
-            exercises.add(new BasicExercise("Ausfallschritte", createExerciseWithThreeSets(2, 12), 2));
-            exercises.add(new BasicExercise("KH-Nackendrücken", createExerciseWithThreeSets(15, 12), 2.5));
-            exercises.add(new BasicExercise("Rückenstrecker", createExerciseWithThreeSets(0, 12), 5));
-            exercises.add(new BasicExercise("Crunches am Gerät", createExerciseWithThreeSets(45, 12), 5));
-            return new BasicWorkout(exercises);
-        }
+        return workouts.get(position);
+    }
 
+    private Workout fakeWorkoutOne() {
+        LinkedList<Exercise> exercises = new LinkedList<>();
+        exercises.add(new BasicExercise("Bankdrücken", createExerciseWithThreeSets(70, 12), 5));
+        exercises.add(new BasicExercise("Schrägbankdrücken", createExerciseWithThreeSets(40, 10), 5));
+        exercises.add(new BasicExercise("Butterfly", createExerciseWithThreeSets(60, 15), 5));
+        exercises.add(new BasicExercise("Liegestütze", createExerciseWithThreeSets(0, 30), 12.5));
+        exercises.add(new BasicExercise("Lat-Ziehen hinter den Nacken", createExerciseWithThreeSets(50, 12), 5));
+        exercises.add(new BasicExercise("Hochziehen der Langhantel mit enger Handstellung", createExerciseWithThreeSets(10, 12), 2.5));
+        exercises.add(new BasicExercise("Klimmzüge", createExerciseWithThreeSets(60, 8), 5));
+        exercises.add(new BasicExercise("Hammercurls", createExerciseWithThreeSets(30, 20), 2.5));
+        exercises.add(new BasicExercise("Larry Scoot Armbeugen", createExerciseWithThreeSets(30, 12), 2.5));
+        exercises.add(new BasicExercise("Situps", createExerciseWithThreeSets(0, 30), 2.5));
+        BasicWorkout workout = new BasicWorkout(exercises);
+        workout.setName("fake workout one");
+        return workout;
+    }
+
+    private Workout fakeWorkoutTwo() {
+        LinkedList<Exercise> exercises = new LinkedList<>();
+        exercises.add(new BasicExercise("Bankdrücken", createExerciseWithThreeSets(50, 12), 5));
+        exercises.add(new BasicExercise("KH-Rudern", createExerciseWithThreeSets(10, 12), 2.5));
+        exercises.add(new BasicExercise("KH-Latissimus", createExerciseWithThreeSets(10, 12), 2.5));
+        exercises.add(new BasicExercise("Triceps am hohen Block", createExerciseWithThreeSets(25, 12), 5));
+        exercises.add(new BasicExercise("Ausfallschritte", createExerciseWithThreeSets(2, 12), 2));
+        exercises.add(new BasicExercise("KH-Nackendrücken", createExerciseWithThreeSets(15, 12), 2.5));
+        exercises.add(new BasicExercise("Rückenstrecker", createExerciseWithThreeSets(0, 12), 5));
+        exercises.add(new BasicExercise("Crunches am Gerät", createExerciseWithThreeSets(45, 12), 5));
+        BasicWorkout workout = new BasicWorkout(exercises);
+        workout.setName("fake workout two");
+        return workout;
     }
 
     @Override
     public String[] getWorkoutlist() {
-        return new String[] {
-                "Workout A",
-                "Workout B",
-                "Workout C",
-                "Workout D",
-                "Workout E"};
+        String[] workoutNames = new String[workouts.size()];
+        for (int i=0;i<workouts.size();i++) {
+            workoutNames[i] = workouts.get(i).getName();
+        }
+        return workoutNames;
     }
 
     private List<Set> createExerciseWithThreeSets(double weight, int reps) {
