adbd36f0253a663cbea5cace592ce94299b5e205
Benjamin <github@avalax.de>
2014-05-03 23:36:24 +0200

    added Tests for WeightDecimalPlaces

--- app/src/main/java/de/avalax/fitbuddy/app/dialog/WeightDecimalPlaces.java
+++ app/src/main/java/de/avalax/fitbuddy/app/dialog/WeightDecimalPlaces.java
@@ -1,21 +1,21 @@
 package de.avalax.fitbuddy.app.dialog;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class WeightDecimalPlaces {
     private static final Map<String, Double> decimalPlaces;
 
     static {
-        decimalPlaces = new HashMap<>();
+        decimalPlaces = new TreeMap<>();
         decimalPlaces.put("0", 0.0);
-        decimalPlaces.put("875", 0.875);
-        decimalPlaces.put("750", 0.75);
-        decimalPlaces.put("675", 0.675);
-        decimalPlaces.put("500", 0.5);
-        decimalPlaces.put("375", 0.375);
-        decimalPlaces.put("250", 0.25);
         decimalPlaces.put("125", 0.125);
+        decimalPlaces.put("250", 0.25);
+        decimalPlaces.put("375", 0.375);
+        decimalPlaces.put("500", 0.5);
+        decimalPlaces.put("625", 0.625);
+        decimalPlaces.put("750", 0.75);
+        decimalPlaces.put("875", 0.875);
     }
 
     public String[] getLabels() {
@@ -29,7 +29,7 @@ public class WeightDecimalPlaces {
 
     public int getPosition(double weight) {
         Double[] doubleValues = decimalPlaces.values().toArray(new Double[decimalPlaces.size()]);
-        for (int i=0;i<doubleValues.length;i++) {
+        for (int i = 0; i < doubleValues.length; i++) {
             if (doubleValues[i] == weight) {
                 return i;
             }
---
+++ app/src/test/java/de/avalax/fitbuddy/app/dialog/WeightDecimalPlacesTest.java
@@ -0,0 +1,68 @@
+package de.avalax.fitbuddy.app.dialog;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class WeightDecimalPlacesTest {
+
+    private WeightDecimalPlaces weightDecimalPlaces;
+
+    private void assertLabel(int position, String label) {
+        String[] labels = weightDecimalPlaces.getLabels();
+        assertThat(labels[position], equalTo(label));
+    }
+
+    private void assertWeight(int position, double weight) {
+        assertThat(weightDecimalPlaces.getWeight(position), equalTo(weight));
+    }
+
+    private void assertPosition(double weight, int position) {
+        assertThat(weightDecimalPlaces.getPosition(weight), equalTo(position));
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        weightDecimalPlaces = new WeightDecimalPlaces();
+    }
+
+    @Test
+    public void testGetLabels() throws Exception {
+        assertThat(weightDecimalPlaces.getLabels().length, is(8));
+        assertLabel(0, "0");
+        assertLabel(1, "125");
+        assertLabel(2, "250");
+        assertLabel(3, "375");
+        assertLabel(4, "500");
+        assertLabel(5, "625");
+        assertLabel(6, "750");
+        assertLabel(7, "875");
+    }
+
+    @Test
+    public void testGetWeight() throws Exception {
+        assertWeight(0, 0.0);
+        assertWeight(1, 0.125);
+        assertWeight(2, 0.25);
+        assertWeight(3, 0.375);
+        assertWeight(4, 0.5);
+        assertWeight(5, 0.625);
+        assertWeight(6, 0.75);
+        assertWeight(7, 0.875);
+    }
+
+    @Test
+    public void testGetPosition() throws Exception {
+        assertPosition(0.0, 0);
+        assertPosition(0.125, 1);
+        assertPosition(0.25, 2);
+        assertPosition(0.375, 3);
+        assertPosition(0.5, 4);
+        assertPosition(0.625, 5);
+        assertPosition(0.75, 6);
+        assertPosition(0.875, 7);
+    }
+}
