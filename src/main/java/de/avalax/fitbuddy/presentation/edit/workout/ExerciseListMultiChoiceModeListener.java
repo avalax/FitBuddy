fcf7c1472a2986d602fc9df6d78b0f65116bcab3
Benjamin <github@avalax.de>
2014-10-03 23:29:48 +0200

    added todo

--- src/main/java/de/avalax/fitbuddy/presentation/edit/workout/ExerciseListMultiChoiceModeListener.java
+++ src/main/java/de/avalax/fitbuddy/presentation/edit/workout/ExerciseListMultiChoiceModeListener.java
@@ -14,6 +14,7 @@ import java.util.Collections;
 import java.util.List;
 
 class ExerciseListMultiChoiceModeListener implements AbsListView.MultiChoiceModeListener {
+    //TODO: introduces itemCheckedListObject with sort,add,remove
     private List<Integer> itemsChecked;
     private ExerciseListFragment exerciseListFragment;
     private EditWorkoutApplicationService editWorkoutApplicationService;
