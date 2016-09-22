38c2cfbfeafa5858178ea2deffcf470dac416d7c
Benjamin <github@avalax.de>
2016-09-22 17:11:09 +0200

    ArrayList to List

--- src/main/java/de/avalax/fitbuddy/port/adapter/service/JsonInWorkoutAdapter.java
+++ src/main/java/de/avalax/fitbuddy/port/adapter/service/JsonInWorkoutAdapter.java
@@ -3,7 +3,6 @@ package de.avalax.fitbuddy.port.adapter.service;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import de.avalax.fitbuddy.domain.model.exercise.BasicExercise;
@@ -14,24 +13,24 @@ import de.avalax.fitbuddy.domain.model.workout.Workout;
 import de.avalax.fitbuddy.domain.model.workout.WorkoutParseException;
 
 public class JsonInWorkoutAdapter {
-    Gson gson = new Gson();
+    private Gson gson = new Gson();
 
     public Workout createFromJson(String contents) throws WorkoutParseException {
         if (contents == null || contents.isEmpty()) {
             throw new WorkoutParseException();
         }
         try {
-            List jsonWorkout = gson.fromJson(contents, ArrayList.class);
-            List<ArrayList> jsonExercises = (List<ArrayList>) jsonWorkout.get(1);
+            List jsonWorkout = gson.fromJson(contents, List.class);
+            List<List> jsonExercises = (List<List>) jsonWorkout.get(1);
             Workout workout = new BasicWorkout();
             workout.setName((String) jsonWorkout.get(0));
 
-            for (ArrayList jsonExercise : jsonExercises) {
+            for (List jsonExercise : jsonExercises) {
                 Exercise exercise = new BasicExercise();
                 exercise.setName((String) jsonExercise.get(0));
 
-                List<ArrayList> jsonSets = (List<ArrayList>) jsonExercise.get(1);
-                for (ArrayList jsonSet : jsonSets) {
+                List<List> jsonSets = (List<List>) jsonExercise.get(1);
+                for (List jsonSet : jsonSets) {
                     double weight = (double) jsonSet.get(1);
                     int maxReps = (int) ((double) jsonSet.get(0));
                     Set set = exercise.createSet();
