184135cdc8ea72fac8ddfe8721254a999e24f998
Benjamin <github@avalax.de>
2016-09-22 18:15:39 +0200

    removed unused import

--- src/main/java/de/avalax/fitbuddy/domain/model/workout/WorkoutException.java
+++ src/main/java/de/avalax/fitbuddy/domain/model/workout/WorkoutException.java
@@ -1,7 +1,5 @@
 package de.avalax.fitbuddy.domain.model.workout;
 
-import java.io.IOException;
-
 import de.avalax.fitbuddy.domain.model.ResourceException;
 
 public class WorkoutException extends ResourceException {
