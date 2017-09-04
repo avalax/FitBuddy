d3d5f4114a261fc6850e721e7a8eed5d388a800d
Benjamin <github@avalax.de>
2017-09-04 13:10:31 +0200

    show initial screen without a workout

--- src/androidTest/java/de/avalax/fitbuddy/FitbuddyAcceptanceTest.java
+++ src/androidTest/java/de/avalax/fitbuddy/FitbuddyAcceptanceTest.java
@@ -17,6 +17,7 @@ import de.avalax.fitbuddy.runner.FitbuddyActivityTestRule;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -37,12 +38,14 @@ public class FitbuddyAcceptanceTest {
             MainActivity.class);
 
     @Test
-    public void initialStart_shouldShowStartFragmentOnly() throws Exception {
+    public void initialStart_shouldShowEmptyStartFragment() throws Exception {
         activityRule.launchActivity();
 
         onView(withId(R.id.navigation_start_item)).check(matches(bottomNavItemIsChecked()));
         onView(withId(R.id.navigation_workout_item)).check(matches(bottomNavItemIsNotChecked()));
         onView(withId(R.id.navigation_statistics_item)).check(matches(bottomNavItemIsNotChecked()));
+        onView(withId(R.id.toolbar_support)).check(matches(isDisplayed()));
+        onView(withText("Click to add workout")).check(matches(isDisplayed()));
     }
 
     @Test
