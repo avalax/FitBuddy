aaae8774f34787d554fecebb16ab7eed7eba46a0
Benjamin <github@avalax.de>
2017-09-24 02:52:17 +0200

    added missing unittest

--- src/test/java/de/avalax/fitbuddy/domain/model/exercise/BasicExercisesTest.java
+++ src/test/java/de/avalax/fitbuddy/domain/model/exercise/BasicExercisesTest.java
@@ -2,10 +2,11 @@ package de.avalax.fitbuddy.domain.model.exercise;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Collections;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
+import static de.avalax.fitbuddy.domain.model.exercise.BasicExerciseBuilder.anExercise;
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class BasicExercisesTest {
     @Test
@@ -16,7 +17,29 @@ public class BasicExercisesTest {
 
         Exercises exercises = new BasicExercises(Collections.singletonList(exercise));
 
-        assertThat(exercises.toString(), equalTo("BasicExercises [exercises=" +
-                "[BasicExercise [name=" + name + "]]]"));
+        assertThat(exercises.toString()).isEqualTo("BasicExercises [exercises=" +
+                "[BasicExercise [name=" + name + "]]]");
+    }
+
+    @Test
+    public void addExercise_shouldBeAddedToExercises() throws Exception {
+        Exercise exercise = anExercise().withName("exercise to add").build();
+        Exercises exercises = new BasicExercises(new ArrayList<>());
+
+        exercises.add(exercise);
+
+        assertThat(exercises).containsOnly(exercise);
+    }
+
+    @Test
+    public void setExercise_shouldReplaceExerciseAtPosition() throws Exception {
+        Exercise exercise = anExercise().withName("old exercise").build();
+        Exercise newExercise = anExercise().withName("new exercise").build();
+        Exercises exercises = new BasicExercises(new ArrayList<>());
+        exercises.add(exercise);
+
+        exercises.set(0, newExercise);
+
+        assertThat(exercises).containsOnly(newExercise);
     }
 }
