618946e8a18f74fd866d2b80686b181a52556896
Benjamin <github@avalax.de>
2018-05-21 16:59:43 +0200

    working on ModelView for SetListFragment

--- src/main/java/de/avalax/fitbuddy/presentation/edit/exercise/EditExerciseViewModel.java
+++ src/main/java/de/avalax/fitbuddy/presentation/edit/exercise/EditExerciseViewModel.java
@@ -1,4 +1,20 @@
 package de.avalax.fitbuddy.presentation.edit.exercise;
 
-public class EditExerciseViewModel {
+import android.app.Activity;
+import android.arch.lifecycle.ViewModel;
+
+import de.avalax.fitbuddy.domain.model.exercise.Exercise;
+
+public class EditExerciseViewModel extends ViewModel {
+    private EditExerciseViewHelper editExerciseViewHelper;
+    private SetAdapter setAdapter;
+
+    public void init(Activity activity, EditExerciseViewHelper editExerciseViewHelper, Exercise exercise) {
+        this.editExerciseViewHelper = editExerciseViewHelper;
+        setAdapter = new SetAdapter(activity, exercise.getSets(), editExerciseViewHelper);
+    }
+
+    public SetAdapter getSetAdapter() {
+        return setAdapter;
+    }
 }
--- src/main/java/de/avalax/fitbuddy/presentation/edit/exercise/SetListFragment.java
+++ src/main/java/de/avalax/fitbuddy/presentation/edit/exercise/SetListFragment.java
@@ -1,6 +1,9 @@
 package de.avalax.fitbuddy.presentation.edit.exercise;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DividerItemDecoration;
 import android.view.LayoutInflater;
@@ -10,18 +13,16 @@ import android.view.ViewGroup;
 import javax.inject.Inject;
 
 import de.avalax.fitbuddy.R;
+import de.avalax.fitbuddy.databinding.EditExerciseBinding;
 import de.avalax.fitbuddy.domain.model.exercise.Exercise;
-import de.avalax.fitbuddy.domain.model.set.Sets;
 import de.avalax.fitbuddy.presentation.FitbuddyApplication;
-import de.avalax.fitbuddy.presentation.welcome_screen.WorkoutRecyclerView;
 
 import static android.support.v7.widget.DividerItemDecoration.VERTICAL;
 
 public class SetListFragment extends Fragment {
 
-    private SetAdapter adapter;
-    private Sets sets;
-    private WorkoutRecyclerView recyclerView;
+    private EditExerciseViewModel viewModel;
+    private EditExerciseBinding binding;
 
     @Inject
     protected EditExerciseViewHelper editExerciseViewHelper;
@@ -29,31 +30,39 @@ public class SetListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_edit_exercise, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.edit_exercise, container, false);
         ((FitbuddyApplication) getActivity().getApplication()).getComponent().inject(this);
-        recyclerView = view.findViewById(android.R.id.list);
-        recyclerView.setEmptyView(view.findViewById(android.R.id.empty));
-        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), VERTICAL));
+        viewModel = ViewModelProviders.of(this).get(EditExerciseViewModel.class);
+        binding.setEditExerciseViewModel(viewModel);
+        binding.setLifecycleOwner(this);
+
+        binding.list.setEmptyView(binding.getRoot().findViewById(android.R.id.empty));
+        binding.list.addItemDecoration(new DividerItemDecoration(getActivity(), VERTICAL));
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
         Exercise exercise = (Exercise) getActivity().getIntent().getSerializableExtra("exercise");
-        sets = exercise.getSets();
-        adapter = new SetAdapter(getActivity(), sets, editExerciseViewHelper);
-        recyclerView.setAdapter(adapter);
-        return view;
+        viewModel.init(getActivity(), editExerciseViewHelper, exercise);
+        binding.list.setAdapter(viewModel.getSetAdapter());
     }
 
     public void notifyItemInserted() {
-        adapter.notifyItemInserted(sets.size() - 1);
-        recyclerView.updateEmptyView();
+        viewModel.getSetAdapter().notifyItemInserted(viewModel.getSetAdapter().getItemCount() - 1);
+        binding.list.updateEmptyView();
     }
 
     public void notifyItemChanged(int position) {
-        adapter.notifyItemChanged(position);
-        recyclerView.updateEmptyView();
+        viewModel.getSetAdapter().notifyItemChanged(position);
+        binding.list.updateEmptyView();
     }
 
     public void removeSelections() {
-        adapter.removeSelections();
-        recyclerView.updateEmptyView();
+        viewModel.getSetAdapter().removeSelections();
+        binding.list.updateEmptyView();
         ((EditExerciseActivity) getActivity()).updateToolbar(0);
     }
 }
